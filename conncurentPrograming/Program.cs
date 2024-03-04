8067122b237de74c6216a41c7b0cb56998587628
Jakub Samek <247781@edu.p.lodz.pl>
2024-03-04 14:15:59 +0100

    commit 1

---
+++ conncurentPrograming/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Main
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+  
+        }
+    }
+}
